3e708a66f2d24d7595f34875dbf51578d318d750
Thomas C <thomas.cosialls@gmail.com>
2020-10-09 04:32:53 +0800

    POST website updated

--- app/Http/Controllers/WebsiteController.php
+++ app/Http/Controllers/WebsiteController.php
@@ -31,56 +31,76 @@ class WebsiteController extends Controller
     {
         $data = $request->validate([
             'name' => 'required|alpha_num|unique:websites|max:50',
-            'airbnb_id' => 'required|integer|unique:listings'
+            'url' => 'required|string'
         ]);
 
+        //Parse airbnb listing ID 
+        if(!Str::of($data['url'])->containsAll(['airbnb', 'rooms']))
+        {
+            return response()->json(['message' => 'Cannot find Airbnb listing from given URL'], 400);
+        }
+
+        //Get airbnb id from URL
+        $slice = Str::of($data['url'])->after('rooms/');
+        $airbnb_id = Str::of($slice)->explode('?')[0];
+
+        $listing = \App\Models\Listing::where('airbnb_id', $airbnb_id)->first();
+
+        if ($listing) 
+        {
+            return response()->json(['message' => 'Listing has been already imported'], 400);
+        }
+
         $client = new \GuzzleHttp\Client();
-        $endpoint = 'https://api.airbnb.com/v1/listings/'.$data['airbnb_id'].'?client_id='.env('AIRBNB_CLIENT_ID');
+        $endpoint = 'https://api.airbnb.com/v1/listings/'.$airbnb_id.'?client_id='.env('AIRBNB_CLIENT_ID');
 
         $response = $client->request('GET', $endpoint);
 
-        if ($response->getStatusCode() == 200)
+        if ($response->getStatusCode() != 200)
         {
-            //Create website
-            $website_data['user_id'] = Auth::id();
-            $website_data['api_id'] = Str::uuid();
-            $website_data['name'] = $data['name'];
-
-            $website = \App\Models\Website::create($website_data);            
-
-            //Create listing
-            $listing_data = json_decode($response->getBody()->getContents(), true);
-
-            $listing = \App\Models\Listing::create([
-                'website_id' => $website->id, 
-                'user_id' => Auth::id(), 
-                'airbnb_id' => $data['airbnb_id'], 
-                'name' => $listing_data['listing']['name'] ?? null, 
-                'picture_sm' => $listing_data['listing']['medium_url'] ?? null, 
-                'picture_xl' => $listing_data['listing']['xl_picture_url'] ?? null, 
-                'price' => $listing_data['listing']['price'] ?? null, 
-                'currency' => $listing_data['listing']['native_currency'] ?? null, 
-                'city'=> $listing_data['listing']['city'] ?? null, 
-                'country'=> $listing_data['listing']['country'] ?? null, 
-                'smart_location'=> $listing_data['listing']['smart_location'] ?? null, 
-                'bathrooms'=> $listing_data['listing']['bathrooms'] ?? null, 
-                'bedrooms'=> $listing_data['listing']['bedrooms'] ?? null, 
-                'beds'=> $listing_data['listing']['beds'] ?? null, 
-                'capacity'=> $listing_data['listing']['person_capacity'] ?? null, 
-                'property_type'=> $listing_data['listing']['property_type'] ?? null, 
-                'room_type'=> $listing_data['listing']['room_type'] ?? null, 
-                'summary'=> $listing_data['listing']['summary'] ?? null, 
-                'description'=> $listing_data['listing']['description'] ?? null, 
-                'space'=> $listing_data['listing']['space'] ?? null, 
-                'neighborhood'=> $listing_data['listing']['neighborhood_overview'] ?? null, 
-                'amenities'=> $listing_data['listing']['amenities'] ?? null, 
-                'checkout_time'=> $listing_data['listing']['check_out_time'] ?? null, 
-                'photos'=> $listing_data['listing']['photos'] ?? null, 
-                'recent_review'=> $listing_data['listing']['recent_review']['review'] ?? null, 
-            ]); 
+            return response()->json(['message' => 'Error while communication with Airbnb'], 400);
         }
 
+        //Create website
+        $website_data['user_id'] = Auth::id();
+        $website_data['api_id'] = Str::uuid();
+        $website_data['name'] = $data['name'];
+
+        $website = \App\Models\Website::create($website_data);            
+
+        //Create listing
+        $listing_data = json_decode($response->getBody()->getContents(), true);
+
+        $listing = \App\Models\Listing::create([
+            'website_id' => $website->id, 
+            'user_id' => Auth::id(), 
+            'airbnb_id' => $airbnb_id, 
+            'name' => $listing_data['listing']['name'] ?? null, 
+            'picture_sm' => $listing_data['listing']['medium_url'] ?? null, 
+            'picture_xl' => $listing_data['listing']['xl_picture_url'] ?? null, 
+            'price' => $listing_data['listing']['price'] ?? null, 
+            'currency' => $listing_data['listing']['native_currency'] ?? null, 
+            'city'=> $listing_data['listing']['city'] ?? null, 
+            'country'=> $listing_data['listing']['country'] ?? null, 
+            'smart_location'=> $listing_data['listing']['smart_location'] ?? null, 
+            'bathrooms'=> $listing_data['listing']['bathrooms'] ?? null, 
+            'bedrooms'=> $listing_data['listing']['bedrooms'] ?? null, 
+            'beds'=> $listing_data['listing']['beds'] ?? null, 
+            'capacity'=> $listing_data['listing']['person_capacity'] ?? null, 
+            'property_type'=> $listing_data['listing']['property_type'] ?? null, 
+            'room_type'=> $listing_data['listing']['room_type'] ?? null, 
+            'summary'=> $listing_data['listing']['summary'] ?? null, 
+            'description'=> $listing_data['listing']['description'] ?? null, 
+            'space'=> $listing_data['listing']['space'] ?? null, 
+            'neighborhood'=> $listing_data['listing']['neighborhood_overview'] ?? null, 
+            'amenities'=> $listing_data['listing']['amenities'] ?? null, 
+            'checkout_time'=> $listing_data['listing']['check_out_time'] ?? null, 
+            'photos'=> $listing_data['listing']['photos'] ?? null, 
+            'recent_review'=> $listing_data['listing']['recent_review']['review'] ?? null, 
+        ]); 
+
         return response()->json(['message' => 'Website successfully created'], 200);
+
     }
 
     /**
