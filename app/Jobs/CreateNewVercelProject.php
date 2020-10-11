56f465f752ff80494789ebe477278dd36f1fa6e8
Thomas C <thomas.cosialls@gmail.com>
2020-10-12 04:26:44 +0800

    add jobs vercel create

---
+++ app/Jobs/CreateNewVercelProject.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Jobs;
+
+use Exception;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use App\Models\Website;
+
+class CreateNewVercelProject implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $website;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(Website $website)
+    {
+        $this->website = $website;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $client = new \GuzzleHttp\Client();
+        $endpoint = 'https://api.vercel.com/v6/projects';
+
+        $response = $client->request('POST', $endpoint,[
+            'headers' => [
+                'Authorization' => 'Bearer '.env('VERCEL_TOKEN')
+            ],
+            'json' => ['name' => $this->website->name]
+        ]);
+
+        if ($response->getStatusCode() != 200)
+        {
+            throw new Exception("Error while creating the project on vercel");
+        }
+
+        $this->website->vercel_project_id = json_decode($response->getBody()->getContents(), true)['id'];
+        $this->website->save();
+
+        $client = new \GuzzleHttp\Client();
+        $endpoint = 'https://api.vercel.com/v6/projects/'.$this->website->vercel_project_id.'/env';
+
+        $response = $client->request('POST', $endpoint,[
+            'headers' => [
+                'Authorization' => 'Bearer '.env('VERCEL_TOKEN')
+            ],
+            'json' => [
+                'type' => 'plain',
+                'key' => 'WEBSITE_API_ID',
+                'value' => $this->website->api_id,
+                'target' => ['production']
+            ]
+        ]);
+
+        $response = $client->request('POST', $endpoint,[
+            'headers' => [
+                'Authorization' => 'Bearer '.env('VERCEL_TOKEN')
+            ],
+            'json' => [
+                'type' => 'plain',
+                'key' => 'API_BASE_URL',
+                'value' => env('APP_URL').'/api',
+                'target' => ['production']
+            ]
+        ]);
+    }
+}
---
+++ app/Jobs/DeployNewSiteVercel.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Jobs;
+
+use Exception;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Storage;
+use App\Models\Website;
+
+class DeployNewSiteVercel implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $website;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(Website $website)
+    {
+        $this->website = $website;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        //Update website status
+        $this->website->status = 'sending_files';
+        $this->website->save();
+
+        $template_name = 'carlton';
+
+        $sha1_array=array();
+        $files = Storage::disk('template_'.$template_name)->allFiles();
+
+        //Upload files on Vercel
+        $client = new \GuzzleHttp\Client();
+        $endpoint = 'https://api.vercel.com/v2/now/files';
+
+        foreach ($files as $key => $file) {
+
+            $sha1_array[] = array(
+                'file' => $file,
+                'sha' => sha1_file(storage_path('templates/'.$template_name.'/').$file),
+                'size' => Storage::disk('template_'.$template_name)->size($file),
+            );
+
+            $response = $client->request('POST', $endpoint,[
+                'headers' => [
+                    'Authorization' => 'Bearer '.env('VERCEL_TOKEN'),
+                    'Content-Length' => Storage::disk('template_'.$template_name)->size($file),
+                    'x-now-digest' => sha1_file(storage_path('templates/'.$template_name.'/').$file),
+                ],
+                'body' => Storage::disk('template_'.$template_name)->get($file)
+            ]);
+
+            if ($response->getStatusCode() != 200)
+            {
+                throw new Exception("Error while posting the files to vercel");
+            }
+        }
+
+        //Update website status
+        $this->website->status = 'deploying';
+        $this->website->save();
+
+        $endpoint = 'https://api.vercel.com/v12/now/deployments';
+        $response = $client->request('POST', $endpoint,[
+            'headers' => [
+                'Authorization' => 'Bearer '.env('VERCEL_TOKEN'),
+            ],
+            'json' => [
+                'name' => $this->website->name,
+                'files' => $sha1_array,
+                "projectSettings" => [
+                    'framework' => 'nextjs',
+                    'devCommand' => null,
+                    'buildCommand' => null,
+                    'outputDirectory' => null,
+                    'rootDirectory' => null,
+                ]
+            ]
+        ]);
+
+        if ($response->getStatusCode() != 200)
+        {
+            throw new Exception("Error while trigerring a deployment on vercel");
+        }
+
+        //Update website status
+        $this->website->status = 'built';
+        $this->website->save();
+    }
+}
