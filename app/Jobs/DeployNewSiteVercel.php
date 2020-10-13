cf0710ac9432d0c60063ff47ea6449c2d141f284
Thomas C <thomas.cosialls@gmail.com>
2020-10-13 18:34:15 +0800

    deploysite job > increase timeout

--- app/Jobs/DeployNewSiteVercel.php
+++ app/Jobs/DeployNewSiteVercel.php
@@ -17,6 +17,9 @@ class DeployNewSiteVercel implements ShouldQueue
 
     protected $website;
 
+    public $tries = 1;
+    public $timeout = 300;
+
     /**
      * Create a new job instance.
      *
