0db47c3b772380d0bf0641579b850bc614d4d8ac
Thomas C <toto@gmail.com>
2020-11-12 18:36:27 +0200

    add/delete domain on vercel ok

---
+++ app/Jobs/DeleteCustomDomain.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Jobs;
+
+use Exception;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use App\Models\Website;
+
+class DeleteCustomDomain implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $website;
+    protected $domain_name;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct($website, $domain_name)
+    {
+        $this->website = $website;
+        $this->domain_name = $domain_name;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $client = new \GuzzleHttp\Client();
+        $endpoint = 'https://api.vercel.com/v1/projects/'.$this->website->vercel_project_id.'/alias?domain='.$this->domain_name.'&teamId='.env('VERCEL_TEAM_ID');
+
+        $response = $client->request('DELETE', $endpoint,[
+            'headers' => [
+                'Authorization' => 'Bearer '.env('VERCEL_TOKEN')
+            ]
+        ]);
+
+        if ($response->getStatusCode() != 200)
+        {
+            throw new Exception("Error while deleting the custom domain on vercel");
+        }
+    }
+}
