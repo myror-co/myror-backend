c3129f873632f491ff31a98519e3c658eceeec91
Thomas C <thomas.cosialls@gmail.com>
2020-11-18 19:32:01 +0200

    add DeleteAccount job

---
+++ app/Jobs/DeleteAccount.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Jobs;
+
+use Exception;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use App\Models\User;
+
+class DeleteAccount implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $user;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $this->user->delete();
+    }
+}
