561e6d168267bd7be77df15ef6138d2f12735e9a
Thomas C <thomas.cosialls@gmail.com>
2021-08-05 18:25:40 -0500

    website models update

--- app/Http/Resources/WebsitePublic.php
+++ app/Http/Resources/WebsitePublic.php
@@ -3,7 +3,7 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
-use App\Http\Resources\Listing as ListingResource;
+use App\Http\Resources\ListingPublic as ListingResource;
 
 class WebsitePublic extends JsonResource
 {
@@ -15,19 +15,28 @@ class WebsitePublic extends JsonResource
      */
     public function toArray($request)
     {
+        $user = \App\Models\User::where('id', $this->user_id)->first();
+
         return [
             'id' => $this->api_id,
             'name' => $this->name,
             'title' => $this->title,
             'icon' => $this->icon ? 'https://'.env('AWS_BUCKET').'.s3.amazonaws.com/'.$this->icon : $this->icon,
             'description' => $this->description,
-            'meta_description' => $this->meta_description,
+            'meta_description' => $this->meta_description ?? "",
+            'cancellation_policy' => $this->cancellation_policy ?? "",
+            'no_show_policy' => $this->no_show_policy ?? "",
+            'deposit_policy' => $this->deposit_policy ?? "",
+            'other_policy' => $this->other_policy ?? "",
             'main_picture' => $this->main_picture,
             'email' => $this->email,
             'phone' => $this->phone,
+            'whatsapp_number' => $this->whatsapp_number,
             'facebook' => $this->facebook,
             'instagram' => $this->instagram,
             'google' => $this->google,
+            'paypal_client_id' => $user->subscribed('default') && $this->paypal_client_id,
+            'branding' => !$user->subscribed('default'),
             'csrf' => csrf_token(),
             'listings' => ListingResource::collection($this->listings)
         ];
