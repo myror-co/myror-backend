ed2e8e3a6ae11007903e3d3e11f1e4809b1db6da
Thomas C <thomas.cosialls@gmail.com>
2021-08-17 20:04:26 -0500

    deploy vercel target production

--- app/Jobs/RedeploySiteVercel.php
+++ app/Jobs/RedeploySiteVercel.php
@@ -81,6 +81,7 @@ class RedeploySiteVercel implements ShouldQueue
             'json' => [
                 'name' => $this->website->name,
                 'files' => $sha1_array,
+                'target' => 'production',
                 "projectSettings" => [
                     'framework' => 'nextjs',
                     'devCommand' => null,
